a0626f02677c2e9246abb8dd4aca3004af8150e9
yerdna1983 <marekzm@azet.sk>
2020-02-05 10:53:44 +0100

    Andrej Test

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -10,43 +10,43 @@
         </tr>
       </thead>
       <tbody>
-          <tr v-for="item of items" :key="item['.key']">
-            <td>{{ item.name }}</td>
-            <td>{{ item.price }}</td>
-            <td>
-                <router-link :to="{ name: 'Edit', params: {id: item['.key']} }" class="btn btn-warning">
-                  Edit
-                </router-link>
-            </td>
-            <td>
-              <button @click="deleteItem(item['.key'])" class="btn btn-danger">Delete</button>
-            </td>
-          </tr>
+        <tr v-for="item of items" :key="item['.key']">
+          <td>{{ item.name }}</td>
+          <td>{{ item.price }}</td>
+          <td>
+            <router-link
+              :to="{ name: 'Edit', params: {id: item['.key']} }"
+              class="btn btn-warning"
+            >Edit</router-link>
+          </td>
+          <td>
+            <button @click="deleteItem(item['.key'])" class="btn btn-danger">Delete</button>
+          </td>
+        </tr>
       </tbody>
     </table>
   </div>
 </template>
 
 <script>
-
-import { db } from '../config/db';
+import { db } from "../config/db";
 
 export default {
   components: {
-      name: 'ListItem'
+    name: "ListItem"
   },
   data() {
     return {
       items: []
-    }
+    };
   },
   firebase: {
-    items: db.ref('items')
+    items: db.ref("items")
   },
   methods: {
     deleteItem(key) {
       this.$firebaseRefs.items.child(key).remove();
     }
   }
-}
+};
 </script>
--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,15 @@
-import Firebase from 'firebase'
- let config = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
-  };
-let app = Firebase.initializeApp(config)
-export const db = app.database()
+import Firebase from "firebase";
+let config = {
+  apiKey: "AIzaSyDX4tr03zNg3_PA2oAPJiiPA_xNET4nYkU",
+  authDomain: "zapisdodb.firebaseapp.com",
+  databaseURL: "https://zapisdodb.firebaseio.com",
+  projectId: "zapisdodb",
+  storageBucket: "zapisdodb.appspot.com",
+  messagingSenderId: "450190089387"
+};
+
+let app = Firebase.apps.length
+  ? Firebase.app()
+  : Firebase.initializeApp(config);
+
+export const db = app.database();
